f14c0c0997623554930ab94b481bbadb18ad0f94
home <akshay.cy2@gmail.com>
2021-05-11 06:12:22 -0400

    commit already

---
+++ script.js
@@ -0,0 +1,52 @@
+/*****************************************
+ *
+ *              ChillSeverus
+ *
+ *          (Entertainment Media)
+ *
+ *            by Akshy Chaudhary
+ *
+ *
+ ******************************************/
+
+class ArtForm {
+    constructor(artName, artGenre, artYear, artDescription, artKeyword) {
+        this.artName = artName;
+        this.artGenre = artGenre;
+        this.artYear = artYear;
+        this.artDescription = artDescription;
+        this.artKeyword = artKeyword;
+    }
+}
+
+const artFormList = [];
+
+const createArt = () => {
+    const artForm = new ArtForm();
+
+    artName = document.getElementById("artName").value;
+
+    artForm.artName = artName;
+
+    let artGenre = document.getElementById("artGenre").value;
+
+    artForm.artGenre = artGenre;
+
+    let artYear = document.getElementById("artYear").value;
+
+    artForm.artYear = artYear;
+
+    let artDescription = document.getElementById("artDescription").value;
+
+    artForm.artDescription = artDescription;
+
+    let artKeyword = document.getElementById("artKeywords").value;
+
+    artForm.artKeyword = artKeywords;
+
+    artFormList.push(artForm);
+
+    let addText = document.getElementById("userResult");
+
+    addText.innerText = `${artName} ${artYear} is added`;
+};
